b494d699da6d9daff99d3b295bb5b1efd494c97b
akshay upadhya <akshaysrinagar@gmail.com>
2017-12-24 23:02:38 +0530

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // Constant with our paths
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
@@ -22,15 +22,23 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(paths.SRC, 'index.html'),
     }),
+    new ExtractTextPlugin('style.bundle.css')
   ],
   module: {
     rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: [
-        'babel-loader',
-      ],
-    }, ],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      }
+    ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
